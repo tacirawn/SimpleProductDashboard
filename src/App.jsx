de5ee9c17de7ef5f3f0755421bb7dd43dafc7d20
Nisa Ayaz <nnisayazz@gmail.com>
2025-12-30 04:54:41 +0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import { useState } from 'react'
 
 function App() {
+    const [showProducts, setShowProducts] = useState(false);
+    const [name, setName] = useState("");
     
+    const toggle = () => {
+        setShowProducts(!showProducts);
+    }
+
+    const nameInput = (event) => {
+        setName(event.target.value);
+    }
+
+    return(
+        <div>
+            <h1>Product Dashboard</h1>
+            <button onClick={toggle}> </button>
+            <input value={name} onChange={nameInput} type='text' placeholder='Enter your name here'></input>
+        </div>
+    );
 }
 
 export default App
